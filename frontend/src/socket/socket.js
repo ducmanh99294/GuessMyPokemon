acb752046a6946fe009b2c62ba481650a6ee4c8c
manh <manh99294@donga.edu.vn>
2026-09-10 23:13:24 +0700

    udt

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+const API_URL = import.meta.env.VITE_API_URL;
 
-const socket = io("http://localhost:5000", {
+const socket = io(API_URL, {
     autoConnect: true
 });
 
--- frontend/src/utils/playerId.js
+++ frontend/src/utils/playerId.js
@@ -1,6 +1,6 @@
 // src/utils/playerId.js
 
-const PLAYER_ID_KEY = "pokemon_guess_player_id";
+const PLAYER_ID_KEY = import.meta.env.PLAYER_KEY;
 
 export function getPlayerId() {
     let playerId = localStorage.getItem(PLAYER_ID_KEY);
